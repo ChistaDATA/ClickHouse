df6b2af7ea73bdce2747964b9143e5407d057375
Max Kainov <max.kainov@clickhouse.com>
2024-10-01 19:19:35 +0000

    CI: FastTest with praktika

--- ci_v2/settings/settings.py
+++ ci_v2/settings/settings.py
@@ -1,8 +1,5 @@
-from ci_v2.settings.definitions import (
-    S3_BUCKET_HTTP_ENDPOINT,
-    S3_BUCKET_NAME,
-    RunnerLabels,
-)
+from ci_v2.settings.definitions import (S3_BUCKET_HTTP_ENDPOINT,
+                                        S3_BUCKET_NAME, RunnerLabels)
 
 S3_ARTIFACT_PATH = f"{S3_BUCKET_NAME}/artifacts"
 CI_CONFIG_RUNS_ON = [RunnerLabels.CI_SERVICES]
--- ci_v2/workflows/pull_request.py
+++ ci_v2/workflows/pull_request.py
@@ -1,12 +1,7 @@
 from typing import List
 
-from ci_v2.settings.definitions import (
-    BASE_BRANCH,
-    DOCKERS,
-    SECRETS,
-    JobNames,
-    RunnerLabels,
-)
+from ci_v2.settings.definitions import (BASE_BRANCH, DOCKERS, SECRETS,
+                                        JobNames, RunnerLabels)
 from praktika import Job, Workflow
 
 style_check_job = Job.Config(
@@ -17,8 +12,8 @@ style_check_job = Job.Config(
 )
 
 fast_test_job = Job.Config(
-    name=JobNames.STYLE_CHECK,
-    runs_on=[RunnerLabels.CI_SERVICES],
+    name=JobNames.FAST_TEST,
+    runs_on=[RunnerLabels.BUILDER],
     command="python3 ./ci_v2/jobs/fast_test.py",
     run_in_docker="clickhouse/fasttest",
 )
@@ -44,9 +39,7 @@ WORKFLOWS = [
 
 
 if __name__ == "__main__":
-    # example: local job test inside praktika environment
+    # local job test inside praktika environment
     from praktika.runner import Runner
 
-    Runner.generate_dummy_environment(workflow, fast_test_job)
-
-    Runner().run(workflow, fast_test_job, docker="fasttest")
+    Runner().run(workflow, fast_test_job, docker="fasttest", dummy_env=True)
